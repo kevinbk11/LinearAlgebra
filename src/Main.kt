4c9d8f1423d1f20ebc2708806e5a0931c4a9e4a4
LoveNyabi <r91090701@gmail.com>
2022-06-14 17:27:59 +0800

    Vector toString

--- src/Main.kt
+++ src/Main.kt
@@ -101,8 +101,8 @@ fun main(args:Array<String>)
         vb.clearAfterCreate=true
         val v1=vb.addElement(3,1).create()
         val v2=vb.addElement(2,2).create()
-        val QR=QR(m11)
-        print(QR)
+
+        print(v1)
         //print(mkv.inverse()*m11.inverse()==(m11*mkv).inverse())
         //print(newMkv*a*(1.0/newMkv[3][1]))
     }
